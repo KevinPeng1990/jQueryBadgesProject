8611dac4d062ea1f7f6963615d60f1dd0d51cd37
Kevin Peng <kevinpeng1990@gmail.com>
2017-03-12 16:24:29 +0800

    Satisfy list of requirements from codeschool.

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,36 @@
-$(function() {
+$(function () {
 
-  // your code will go here
+  $.ajax({
+    url: 'https://www.codeschool.com/users/2685101.json',
+    dataType: 'jsonp',
+    success: function (response) {
+      generatedBadges(response.courses.completed)
+    }
+  });
+
+  function generatedBadges(courses) {
+    var $badges = $('#badges');
+
+    courses.forEach(function (course) {
+      $div = $('<div />', {
+        'class': 'course'
+      }).appendTo($badges);
+
+      $('<h3 />', {
+        text: course.title
+      }).appendTo($div);
+
+      $('<img />', {
+        src: course.badge
+      }).appendTo($div);
+
+      $('<a />', {
+        'class': 'btn btn-primary',
+        href: course.url,
+        target: '_blank',
+        text: 'See Course'
+      }).appendTo($div);
+    });
+  }
 
 });
